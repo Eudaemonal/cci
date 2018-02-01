59a004ff4426b7e95d53ec858562e7d5c4218b78
eudaemonal <eudaemonal@gmail.com>
2018-02-01 20:20:15 +0800

    update rasom note

--- ransom_note/test01.cpp
+++ ransom_note/test01.cpp
@@ -1,37 +1,19 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <string>
 #include <unordered_map>
 
-using namespace std;
 
-unordered_map<string,int>::iterator it;
+
+std::unordered_map<std::string,int>::iterator it;
 
 // Put every word from the magazine in a hash table, check if every word from the ransom is in the hash table
-bool ransom_note(vector<string> magazine, vector<string> ransom) {
-	unordered_map<string, int> map;
+bool ransom_note(std::vector<std::string> magazine, std::vector<std::string> ransom){
+    std::unordered_map<std::string, int> map;
     for(int i = 0; i < magazine.size(); i++) {
     	it = map.find(magazine[i]);
     	if(it == map.end()) {
-    		map.insert(pair<string, int>(magazine[i], 1));
+    		map.insert(std::pair<std::string, int>(magazine[i], 1));
     	} else {
     		map[magazine[i]]++;
     	}
@@ -51,18 +33,18 @@ bool ransom_note(vector<string> magazine, vector<string> ransom) {
 int main(){
     int m;
     int n;
-    cin >> m >> n;
-    vector<string> magazine(m);
+    std::cin >> m >> n;
+    std::vector<std::string> magazine(m);
     for(int magazine_i = 0;magazine_i < m;magazine_i++){
-       cin >> magazine[magazine_i];
+       std::cin >> magazine[magazine_i];
     }
-    vector<string> ransom(n);
+    std::vector<std::string> ransom(n);
     for(int ransom_i = 0;ransom_i < n;ransom_i++){
-       cin >> ransom[ransom_i];
+       std::cin >> ransom[ransom_i];
     }
     if(ransom_note(magazine, ransom))
-        cout << "Yes\n";
+        std::cout << "Yes\n";
     else
-        cout << "No\n";
+        std::cout << "No\n";
     return 0;
 }
