95f01ef15ccc8720851e1e237bc7053447ee8372
eudaemonal <eudaemonal@gmail.com>
2018-02-28 20:33:41 +1100

    finsh dfs

--- Shortest_Reach_in_a_Graph/test01.cpp
+++ Shortest_Reach_in_a_Graph/test01.cpp
@@ -1,27 +1,72 @@
 #include <iostream>
 #include <vector>
-
+#include <set>
+#include <list>
+#include <algorithm>
 
 class Graph{
 public:
 	Graph(int n)
-		: num_nodes{n};
+		: num_nodes{n}
+	{
+		adjacency_list.resize(n);
+		//std::cout << "num nodes: " << num_nodes << "\n";
+	}
 
 
 	void add_edge(int u, int v){
-		auto p = std::make_pair(u,v);
-		list.push_back(p);
+		u--;
+		v--;
+		adjacency_list[u].insert(v);
+		adjacency_list[v].insert(u);
+
+		//std::cout << "Add Edge: " << u << ", " << v << "\n";
 	}
 
 	std::vector<int> shortest_reach(int start){
-		int i = 0;
-		while(i < num_nodes){
-			
-			
+		start--;
+
+		std::vector<int> v;
+		bool visited[num_nodes];
+		int distance[num_nodes];
+		for(int i = 0; i<num_nodes;++i){
+			visited[i] = false;
+			distance[i] = 0;
 		}
+
+		std::list<int> queue;
+		
+		queue.push_back(start);
+
+		visited[start] = true;
+
+		while(!queue.empty()){
+			int node = queue.front();
+			for(auto pos = adjacency_list[node].begin(); pos!=adjacency_list[node].end();++pos){
+				if(!visited[*pos]){
+					queue.push_back(*pos);
+					distance[*pos] += (distance[node] + 6);
+					visited[*pos] = true;
+				}
+			}
+			queue.pop_front();
+		}
+
+		for(int i = 0; i < num_nodes; i++) {
+			//std::cout << i << " dis: "<<distance[i] << "\n";
+            if(i != start) {
+                if(distance[i] == 0) {
+                    v.push_back(-1);
+                } else {
+                    v.push_back(distance[i]);
+                }
+            }
+        }
+        return v;
 	}
+
 private:
-	std::vector<std::pair<int, int>> list;
+	std::vector<std::set<int>> adjacency_list;
 	int num_nodes;
 };
 
@@ -40,19 +85,16 @@ int main(){
 		for(int i = 0; i < m; ++i){
 			int u, v;
 			std::cin >> u >> v;
-			u--; v--;
+			
 			graph.add_edge(u,v);
 		}
 		int startId;
 		std::cin >> startId;
-		startId--;
 
 		std::vector<int> distances = graph.shortest_reach(startId);
 
 		for(int i = 0;i< distances.size(); ++i){
-			if(i!=startId){
-				std::cout << distancs[i] << " ";
-			}
+				std::cout << distances[i] << " ";
 		}
 		std::cout << "\n";
 		
