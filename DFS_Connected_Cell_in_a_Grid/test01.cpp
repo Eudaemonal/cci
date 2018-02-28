b491e018d10f18f91bce1265618b86eb7937e54c
eudaemonal <eudaemonal@gmail.com>
2018-02-28 20:44:02 +1100

    start file

--- DFS_Connected_Cell_in_a_Grid/test01.cpp
+++ DFS_Connected_Cell_in_a_Grid/test01.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 
+int get_biggest_region(std::vector<std::vector<int>> grid){
 
+}
 
+int main(){
+	int n, m;
+	std::cin >> n;
+	std::cin >> m;
+
+	std::vector<std::vector<int>> grid(n, std::vector<int>(m));
+
+	for(int grid_i = 0; grid_i < n; ++grid_i){
+		for(int grid_j =0; grid_j < m; ++grid_j){
+			std::cin >> grid[grid_i][grid_j];
+		}
+	}
+
+	std::cout << get_biggest_region(grid) << "\n";
+	return 0;
+}
