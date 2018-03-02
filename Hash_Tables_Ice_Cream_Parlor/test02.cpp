cbdd30fb0b5d69add4f395ea3b2789a28c31b0f1
eudaemonal <eudaemonal@gmail.com>
2018-03-02 18:20:22 +1100

    update

--- Hash_Tables_Ice_Cream_Parlor/test02.cpp
+++ Hash_Tables_Ice_Cream_Parlor/test02.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
-std::vector<int> make_choose(int money, std::vector<int> v){
-}
 
 int main(){
 	int trip;
@@ -13,15 +12,56 @@ int main(){
 		std::vector<int> v, r;
 		std::cin >> money;
 		std::cin >> n;
+
+		std::map<int, std::vector<int>> price;
 		for(int i = 0; i < n; ++i){
-			std::cin >> elem;
-			v.push_back(elem);
+			int flavor;
+			std::cin >> flavor;
+			if(flavor >= money){
+				continue;
+			}
+			if(price.count(flavor) == 0){
+				price[flavor] = std::vector<int>(1,i);
+			}
+			else{
+				price[flavor].push_back(i);
+			}
 		}
-		r = make_choose(money, v);
-		for(int i = 0; i < 2; ++i){
-			std::cout << r[i] << " ";
+		int half = money / 2;
+		auto middle = price.lower_bound(half);
+		auto bottom = price.begin();
+		auto top = price.upper_bound(half);
+		int one;
+		int two;
+		if(2*half == money && middle->first == half && middle->second.size() >=2)
+		{
+			one = middle->second[0];
+			two = middle->second[1];
+		}else{
+			while(bottom!=middle && top!=price.end()){
+				bottom = price.lower_bound(money - top->first);
+				if(bottom->first + top->first == money)
+					break;
+				else if(bottom!=price.begin())
+					bottom--;
+
+				top = price.lower_bound(money - bottom->first);
+				if(bottom->first + top->first == money)
+					break;
+			}
+			one = bottom->second[0];
+			two = top->second[0];
 		}
-		std::cout << "\n";
+
+		one++;
+		two++;
+		if(one < two){
+			std::cout << one << " " << two << "\n";
+		}else{
+			std::cout << two << " " << one << "\n";
+		}
+
+
 	}
 }
 
