5115b1fae6b12a6a56b6534b539fc0b5bab8f907
eudaemonal <eudaemonal@gmail.com>
2018-05-11 10:08:09 +1000

    update

--- Integer_to_Roman/test01.cpp
+++ Integer_to_Roman/test01.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+std::string convert(int n){
+	std::vector<std::string> M = {"", "M", "MM", "MMM"};
+	std::vector<std::string> C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+	std::vector<std::string> X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+	std::vector<std::string> I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+	return M[n/1000] + C[(n%1000)/100] + X[(n%100)/10] + I[n%10];
+}
+
+
 
 int main(int argc, char *argv[]){
+	int n;
+	std::cin >> n;
+	
+	std::cout << convert(n) << "\n";
 
 	return 0;
 }
